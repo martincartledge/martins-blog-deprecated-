7bf4fa20c0f9eb8574ad9b9a9b721b78aa3a9286
Martin Cartledge <mcartledge@gogoair.com>
2020-04-12 16:06:43 -0600

    add signup form import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import SignUpForm from "../components/signupForm"
 import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
